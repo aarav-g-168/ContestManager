c09fc33e876ead692bd7b6e32ab35e4f2cf88653
aarav-g-168 <guptaaarav987@gmail.com>
2026-03-12 16:44:33 +0530

    created a new route for all upcoming contests

---
+++ src/app/contests/page.tsx
@@ -0,0 +1,15 @@
+import type { Contest } from "../types/contest";
+
+export default function ContestsPage() {
+  return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-4xl font-bold text-center mb-8 text-blue-600">
+        All Upcoming Contests
+      </h1>
+
+      <p className="text-center text-gray-600">
+        
+      </p>
+    </div>
+  );
+}
