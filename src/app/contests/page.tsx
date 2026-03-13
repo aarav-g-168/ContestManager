fb53aaf59fc34fc6e5998952c254f28ab6500f34
aarav-g-168 <guptaaarav987@gmail.com>
2026-03-13 21:52:05 +0530

    made some minor changes

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -3,7 +3,7 @@ import type { Contest } from "../types/contest";
 export default function ContestsPage() {
   return (
     <div className="bg-gray-100 min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-center mb-8 text-blue-600">
+      <h1 className="text-4xl font-bold text-center mb-8 text-gray-600">
         All Upcoming Contests
       </h1>
 
