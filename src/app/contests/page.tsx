bf8f07d549a705a25e413b26b2193ea1ac2ee8c0
aarav-g-168 <guptaaarav987@gmail.com>
2026-03-15 22:32:52 +0530

    fetched all the contests in the new route

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -1,15 +1,84 @@
+import ContestClientComponent from "@/src/app/ContestClientComponent";
 import type { Contest } from "../types/contest";
+import { parseAsUTC } from "../lib/date";
 
-export default function ContestsPage() {
+export const dynamic = "force-dynamic";
+
+interface GetContestsResponse {
+  contests?: Contest[];
+  error?: string;
+}
+
+const API_KEY = process.env.NEXT_PUBLIC_CLIST_API_KEY;
+
+async function getContests(): Promise<GetContestsResponse> {
+  if (!API_KEY) {
+    return { error: "API Key is not configured on the server." };
+  }
+
+  const now = new Date().toISOString();
+  const future = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString();
+
+  const API_URL =
+    `https://clist.by/api/v4/contest/?start__gte=${now}&start__lte=${future}&order_by=start`;
+
+  try {
+    const response = await fetch(API_URL, {
+      headers: {
+        Authorization: `ApiKey ${API_KEY}`,
+      },
+      cache: "no-store",
+    });
+
+    const data = await response.json();
+
+    return { contests: data.objects };
+  } catch (err) {
+    return { error: "Failed to fetch contests" };
+  }
+}
+
+export default async function ContestsPage() {
+  const { contests, error } = await getContests();
+
+  const allContests = contests ?? [];
+
+  const contestsWithFormattedStart = allContests.map((c: Contest) => ({
+    ...c,
+    formattedStartIST:
+      parseAsUTC(c.start).toLocaleString("en-IN", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        timeZone: "Asia/Kolkata",
+      }) + " (IST)",
+  }));
+
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-center mb-8 text-gray-600">
-        All Upcoming Contests
-      </h1>
+    <div className="bg-gray-100 min-h-screen font-sans">
+      <div className="container mx-auto p-4 md:p-8">
 
-      <p className="text-center text-gray-600">
-        All the upcoming programming contests from various platforms in one place. Stay tuned.
-      </p>
+        <header className="text-center mb-8">
+          <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-3 mt-7">
+            All Upcoming Contests
+          </h1>
+        </header>
+
+        {error ? (
+          <div className="text-center bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg">
+            <p className="font-bold">An Error Occurred</p>
+            <p>{error}</p>
+          </div>
+        ) : (
+          <ContestClientComponent
+            initialContests={contestsWithFormattedStart}
+          />
+        )}
+
+      </div>
     </div>
   );
 }
