9d46c6f924a55cfc4b5c393d0888e4ad29f21b8f
aarav-g-168 <guptaaarav987@gmail.com>
2026-05-26 13:05:27 +0530

    fixed hydation issue

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,9 +2,21 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Hydration fix
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
@@ -16,7 +28,7 @@ export default function ThemeToggle() {
       {theme === "dark" ? (
         <Sun className="h-5 w-5 text-yellow-400" />
       ) : (
-        <Moon className="h-5 w-5 text-gray-800" />
+        <Moon className="h-5 w-5 text-gray-800 dark:text-white" />
       )}
     </button>
   );
