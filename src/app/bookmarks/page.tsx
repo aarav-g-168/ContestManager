5b2f06ec1ab773b3af17287f92ac30b490510048
aarav-g-168 <guptaaarav987@gmail.com>
2026-06-03 23:08:59 +0530

    fixed the deployement issue

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import ContestClientComponent from "@/components/ContestClientComponent";
 import Navbar from "@/components/Navbar";
 import { getContests } from "@/lib/getContests";
