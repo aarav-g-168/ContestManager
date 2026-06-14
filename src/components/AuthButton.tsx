4cc3b32a697d9eb948eb831c806282917062be0d
aarav-g-168 <guptaaarav987@gmail.com>
2026-06-14 22:17:34 +0530

    made some UI changes

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -65,7 +65,7 @@ export default function AuthButton() {
       onClick={handleLogin}
       className="bg-indigo-600 hover:bg-indigo-500 text-white px-4 py-2 rounded-xl text-sm font-medium transition-all duration-200"
     >
-      Login
+      Get Started
     </button>
   );
 }
